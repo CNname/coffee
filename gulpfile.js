aa1d20cd1b4f973008a302e95ca86336d269ed73
CNname <howling.jpg.exe@gmail.com>
2017-04-04 14:11:10 +0300

    added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+var ts = require('gulp-typescript');
+
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        },
+    });
+});
 
 gulp.task('html', function(){
   return gulp.src(['./dev/index.html'])
@@ -14,35 +23,34 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest('./dist/js'));
 });
 
+gulp.task('typescript', function(){
+  return gulp.src('./dev/**/*.ts')
+          .pipe(ts({
+              noImplicitAny: true,
+              out: 'output.js'
+          }))
+          .pipe(gulp.dest('./dist/js'));
+});
+
 gulp.task('sass', function () {
   return gulp.src('./dev/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('sass:watch', function () {
-  livereload.listen();
-  gulp.watch('./dev/sass/**/*.scss', ['sass']);
-});
-
-gulp.task('coffee:watch', function() {
-  livereload.listen();
-  gulp.watch('./dev/*.coffee', ['coffee']);
-})
-
-gulp.task('html:watch', function(){
-  livereload.listen();
-  gulp.watch('./dev/index.html', ['html']);
-});
 
 gulp.task('assets', function(){
   return gulp.src(['./dev/assets/**/*.*'])
   .pipe(gulp.dest('./dist/assets'));
 });
 
-gulp.task('assets:watch', function(){
-  livereload.listen();
+gulp.task('watch', ['browserSync', 'sass', 'typescript'], function(){
+  gulp.watch('./dev/**/*.html').on('change', browserSync.reload)
+  gulp.watch('./dev/sass/**/*.scss', ['sass']);
   gulp.watch(['./dev/assets/**/*.*'], ['assets'])
-});
+  gulp.watch('./dev/**/*.coffee', ['coffee'], browserSync.reload);
+  gulp.watch('./dev/**/*.ts', ['typescript'], browserSync.reload);
+})
 
-gulp.task('default', ['html', 'sass', 'coffee', 'sass:watch', 'coffee:watch', 'html:watch', 'assets', 'assets:watch']);
+//gulp.task('default', ['html', 'sass', 'coffee', 'sass:watch', 'coffee:watch', 'html:watch', 'assets', 'assets:watch']);
+gulp.task('default', ['watch']);
