ff26971b8afaadf436378cbec9d177cebbdd2f4e
perttu <savolainen.perttu@gmail.com>
2017-04-07 10:03:51 +0300

    fixed tsconfig so it compiles everything to single file

--- dev/main.ts
+++ dev/main.ts
@@ -1,8 +1,15 @@
-import { CanvasHandler } from './canvas-handler.class';
-
+// import { CanvasHandler } from './canvas-handler.class';
+declare function require(param1: string[], param2: Function): any;
 console.log("moi");    
 
-let t = new CanvasHandler('aco-canvas');
-t.getCtx().moveTo(0,0);
-t.getCtx().lineTo(200,200);
-t.getCtx().stroke();
+require(['./canvas-handler.class'], (res) => {
+
+    const CanvasHandler = res.CanvasHandler;
+
+    let t = new CanvasHandler('aco-canvas');
+
+    t.getCtx().moveTo(0,0);
+    t.getCtx().lineTo(200,200);
+    t.getCtx().stroke();
+
+});
