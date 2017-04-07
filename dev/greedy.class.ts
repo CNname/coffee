d60d60be5d4c5b02e60a12c28069a45714d7bd54
perttu <savolainen.perttu@gmail.com>
2017-04-07 19:07:48 +0300

    created canvas arrow and connection to other points

--- dev/greedy.class.ts
+++ dev/greedy.class.ts
@@ -15,28 +15,28 @@ export class GreedyAlgorithm {
     /**
      *
      * @param pointArray
-     * @param currentPosition
-     * @returns {number} index of closest point
+     * @param currentCoords
+     * @returns {{index: number, distance: number}}
      */
-    findClosestPoint(pointArray: Array<{ x: number; y: number; }>, currentPosition: number): number {
+    findClosestPoint(pointArray: Array<{ x: number; y: number; }>, currentCoords: { x: number; y:number; }): { index: number; distance: number; } {
 
-        const currentX: number = pointArray[currentPosition].x;
-        const currentY: number = pointArray[currentPosition].y;
+        const currentX: number = currentCoords.x;
+        const currentY: number = currentCoords.y;
         let distArr: Array<{index: number; distance: number; }> = [];
 
         for (let i=0; i<pointArray.length; i++) {
-            if (i !== currentPosition) {
-                // get the subtraction of current distance and another
-                let subX = Math.abs(currentX - pointArray[i].x);
-                let subY = Math.abs(currentY - pointArray[i].y);
-                // get the distance using pythagoras theorem
-                let distance = Math.sqrt(Math.pow(subX, 2) + Math.pow(subY, 2));
 
-                distArr.push({
-                    index: i,
-                    distance: distance
-                });
-            }
+            // get the subtraction of current distance and another
+            let subX: number = Math.abs(currentX - pointArray[i].x),
+                subY: number = Math.abs(currentY - pointArray[i].y);
+            // get the distance using pythagoras theorem
+            let distance = Math.sqrt(Math.pow(subX, 2) + Math.pow(subY, 2));
+
+            distArr.push({
+                index: i,
+                distance: distance
+            });
+
         }
 
         // creates own sort function which compares objects distance and sorts them
@@ -44,7 +44,7 @@ export class GreedyAlgorithm {
             return a.distance - b.distance;
         });
 
-        return distArr[0].index;
+        return distArr[0];
 
     }
 
@@ -54,15 +54,58 @@ export class GreedyAlgorithm {
      */
     drawPath(ctx: any) {
 
-        let homeCoords = this.coords[this.home];
-        let closest = this.coords[this.findClosestPoint(this.coords, this.home)];
+        // deep copy an array
+        let coordsArray = JSON.parse(JSON.stringify(this.coords));
+        // delete home from coordsArray
+        coordsArray.splice(this.home, 1);
+        let current = this.coords[this.home];
 
-        ctx.moveTo(homeCoords.x, homeCoords.y);
-        ctx.lineTo(closest.x, closest.y);
+        let totalDistance: number = 0;
+
+        while (coordsArray.length > 0) {
+            let closestPoint = this.findClosestPoint(coordsArray, current);
+            let closest = coordsArray[closestPoint.index];
+            this.canvas_arrow(ctx, current.x, current.y, closest.x, closest.y);
+            totalDistance += closestPoint.distance; // add distance to totalDistance
+            current = closest;
+            coordsArray.splice(closestPoint.index, 1);
+        }
+
+        // finally return home
+        this.canvas_arrow(ctx, current.x, current.y, this.coords[this.home].x, this.coords[this.home].y);
+
+        // when every line is drawn, use stroke
         ctx.strokeStyle = '#17192A';
         ctx.stroke();
+
+        // print total distance
+        ctx.fillStyle = "#393b5b";
+        ctx.font = "14px Arial";
+        ctx.fillText("Kokonaispituus: " + Math.round(totalDistance), 10, 24);
+
     }
 
+    /**
+     * Draw a line with arrow
+     * @param context
+     * @param fromx
+     * @param fromy
+     * @param tox
+     * @param toy
+     */
+    canvas_arrow(context: any, fromx: number, fromy: number, tox: number, toy: number){
 
+        let headlen = 5;   // length of head in pixels
+        let angle = Math.atan2(toy-fromy,tox-fromx);
+        let endx = tox-(headlen*2)*Math.cos(angle);
+        let endy = toy-(headlen*2)*Math.sin(angle);
+
+        context.moveTo(fromx, fromy);
+        context.lineTo(endx, endy);
+        context.lineTo(endx-headlen*Math.cos(angle-Math.PI/6),endy-headlen*Math.sin(angle-Math.PI/6));
+        context.moveTo(endx, endy);
+        context.lineTo(endx-headlen*Math.cos(angle+Math.PI/6),endy-headlen*Math.sin(angle+Math.PI/6));
+
+    }
 
 }
