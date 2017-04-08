20163252787aa2ecdc47e94680775d6c65bf37a0
CNname <howling.jpg.exe@gmail.com>
2017-04-08 18:28:23 +0300

    modified aco class

--- dev/aco.class.ts
+++ dev/aco.class.ts
@@ -5,50 +5,56 @@ import { Ant } from "./ant.class";
  */
 export interface Colony {
     popSize: number;
-    maxIteration: number;
-    distances: number;
+    maxIterations: number;
+    distances: Array<Array<number>>;
     alpha: number;
     beta: number;
     pho: number
     q: number;
     ip: number;
     population: Array<Ant>;
-    pheromones: Array<number>;
+    pheromone:number;
+    pheromones: Array<Array<number>>;
     bestLength: number;
     bestSolution: number;
     goOn: boolean; // continue
     onNewBest: number;
     setOnNewBest(): void;
     setOnIteration(): void;
-    init(): void;
+    init(coords:any): void;
     iterate(): void;
     doWork(): void;
     sendOutAnts(): void;
     updatePheromones(): void;
     evaporatePheromones(): void;
-    daemonActions(): void;
+    daemonActions(x:number): void;
 }
 
 export class AcoAlgorithm implements Colony {
-
-    popSize: number;
-    maxIteration: number;
-    distances: number;
+    popSize: number; // size of the colony
+    maxIterations: number;
+    distances: Array<Array<number>>;
     alpha: number;
     beta: number;
     pho: number;
     q: number;
     ip: number;
-    population: Array<Ant>;
-    pheromones: Array<number>;
+    population: Array<Ant>; // ants of the colony
+    pheromone:number;
+    pheromones: Array<Array<number>>;
     bestLength: number;
     bestSolution: number;
     goOn: boolean;
     onNewBest: number;
 
-    constructor() {
+    constructor(alpha:number, beta:number, q:number, iterations:number, pheromone:number) {
         // just for testing an Ant class
         this.population = [];
+        this.alpha = alpha;
+        this.beta = beta;
+        this.q = q;
+        this.maxIterations = iterations;
+        this.pheromone = pheromone;
     }
 
     setOnNewBest(): void {
@@ -57,34 +63,89 @@ export class AcoAlgorithm implements Colony {
     setOnIteration(): void {
     }
 
-    init(): void {
+    init(coords:any): void {
         this.popSize = 20; // the amount of ants
         this.pheromones = [];
         this.bestSolution = null;
         this.goOn = true;
+        this.calculateDistances(coords);
+
         for(let i = 0; i < this.popSize; i++) {
-            this.population.push(new Ant()); 
+            this.population.push(new Ant(this.alpha, this.beta, this.q)); 
         }
+
+        for(let x = 0; x < this.distances.length; x++) {
+            this.pheromones[x] = [];
+            for(let y = 0; y < this.distances.length; y++) {
+                if (x !== y) {
+                this.pheromones[x][y] = this.pheromone;
+                }
+            }
+        }
+        console.log('Pheromones');
+        console.log(this.pheromones);
     }
 
     iterate(): void {
+        let z = 0;
+        let update = ((z)=>{
+            setTimeout(()=>{
+                //this.sendOutAnts();
+                //this.updatePheromones();
+                z++;
+                //this.daemonActions(z);
+                if (z < this.maxIterations && this.goOn) {
+                    console.log('iteration ' + z);
+                    update(z);
+                }
+            }, 500);
+        });
+        update(z);
     }
 
     doWork(): void {
     }
 
     sendOutAnts(): void {
+        for(let i = 0; i < this.popSize; i++) {
+            this.population[i].doWalk(this.distances, this.pheromones);
+        }
     }
 
     updatePheromones(): void {
+        this.evaporatePheromones();
+        for(let i = 0; i < this.popSize; i++) {
+            this.population[i].layPheromones(this.pheromones);
+        }
     }
 
     evaporatePheromones(): void {
+        for(let x = 0; x < this.distances.length; x++) {
+            for(let y = 0; y < this.distances.length; y++) {
+                if (x !== y) {
+                    this.pheromones[x][y] = (1 - this.pho) * this.pheromones[x][y];
+                } 
+            }
+        }
     }
 
-    daemonActions(): void {
+    daemonActions(x:number): void {
     }
 
+    calculateDistances(dist:Array<any>){
+       this.distances = [];
+       for (let i = 0; i < dist.length; i++) {
+            this.distances[i] = [];
+            for (let j = 0; j < dist.length; j++) {
+                let subX: number = dist[i].x - dist[j].x,
+                subY: number = dist[i].y - dist[j].y;
+                if (i !== j) {
+                    let distance = Math.sqrt(Math.pow(subX, 2) + Math.pow(subY, 2));
+                    this.distances[i][j] = distance;
+                }
+            }
+        }
+    }
 
 
 }
--- dev/main.ts
+++ dev/main.ts
@@ -10,7 +10,7 @@ require(['./canvas-handler.class', './greedy.class', './aco.class'], (ch, g, a) 
     const CanvasHandler = ch.CanvasHandler;
     const GreedyAlgorithm = g.GreedyAlgorithm;
     const AcoAlgorithm = a.AcoAlgorithm;
-
+    let lull:Function;
     let acoCanvas = new CanvasHandler('aco-canvas');
     let greedyCanvas = new CanvasHandler('greedy-canvas');
     const CANVAS_WIDTH: number = 600;
@@ -42,6 +42,7 @@ require(['./canvas-handler.class', './greedy.class', './aco.class'], (ch, g, a) 
     let cityConfig: { amount?: number; coords?: Array<Coord>; home?: number } = {};
     cityConfig.amount = 30;
     cityConfig.coords = getCoords(cityConfig.amount);
+
     cityConfig.home = Math.round(Math.random() * cityConfig.amount);
 
     for (let coord of cityConfig.coords) {
@@ -56,11 +57,21 @@ require(['./canvas-handler.class', './greedy.class', './aco.class'], (ch, g, a) 
         }
 
     }
-
+    // create greedy algorithm
     let greedy = new GreedyAlgorithm(cityConfig.home, cityConfig.coords);
     greedy.drawPath(greedyCanvas.getCtx());
 
-    let aco = new AcoAlgorithm();
-    aco.init();
+    // Create ant colony
+    let alpha = 1.0,
+        beta = 2.0,
+        q = 1.0,
+        iterations = 100,
+        pheromone = 1.0;
+
+    let aco = new AcoAlgorithm(alpha, beta, q, iterations, pheromone);
+    aco.init(JSON.parse(JSON.stringify(cityConfig.coords)));
+    //aco.calculateDistances();
+    //console.log(cityConfig.coords[0]);
+    //aco.iterate();
 
 });
